ffe936f3d5e7c1df301c4ddff4845e5c3575622f
mikava7 <i.mikava365@gmail.com>
2023-10-10 09:48:42 +0200

    scroll down by feture

--- src/pages/Projects/Descriptions.jsx
+++ src/pages/Projects/Descriptions.jsx
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState, useRef, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
 import GithubIcon from "../../assets/github.svg";
 import { projectDescription } from "../../components/projectDescription";
+import { FaArrowDown } from "react-icons/fa";
 
 const Descriptions = () => {
   const [expand, setExpand] = useState(false);
+  const scrollRef = useRef(null);
 
   const toggleExpand = () => {
     setExpand(!expand);
   };
+
+  const scrollDown = () => {
+    if (scrollRef.current) {
+      const featureHeight = scrollRef.current.firstElementChild.clientHeight;
+      scrollRef.current.style.scrollBehavior = "smooth";
+      scrollRef.current.scrollBy(0, featureHeight);
+    }
+  };
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.addEventListener("scroll", () => {
+        if (scrollRef.current.style.scrollBehavior === "smooth") {
+          scrollRef.current.style.scrollBehavior = "auto";
+        }
+      });
+    }
+  }, []);
 
   return (
     <Container>
@@ -16,7 +36,7 @@ const Descriptions = () => {
         <ProjectCard key={project.title}>
           <ProjectTitle>{project.title}</ProjectTitle>
           <ProjectDescription>{project.description}</ProjectDescription>
-          <FeatureList>
+          <FeatureList ref={scrollRef}>
             {project?.features
               .slice(0, expand ? project.features.length : 2)
               .map((feature) => (
@@ -38,29 +58,34 @@ const Descriptions = () => {
                 </a>
               </ProjectLink>
             )}
+            <IconWrapper>
+              <GithubIconImg src={GithubIcon} alt="GitHub" />
+              <a
+                href="https://github.com/your-github-repo-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              ></a>
+            </IconWrapper>
             {project.features.length > 2 && (
               <ExpandButton onClick={toggleExpand}>
                 {expand ? "Show Less" : "Show More"}
               </ExpandButton>
             )}
           </ProjectLinks>
+          {expand && (
+            <ScrollIndicator onClick={scrollDown}>
+              <FaArrowDown />
+            </ScrollIndicator>
+          )}
         </ProjectCard>
       ))}
-      <IconWrapper>
-        <GithubIconImg src={GithubIcon} alt="GitHub" />
-        <a
-          href="https://github.com/your-github-repo-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GoToGitHubButton>Go to GitHub</GoToGitHubButton>
-        </a>
-      </IconWrapper>
     </Container>
   );
 };
 
 export default Descriptions;
+
+// Styled components...
 
 const Container = styled.div`
   width: 100%;
@@ -73,20 +98,13 @@ const Container = styled.div`
   border-radius: 12px;
   /* outline: 1px solid red; */
 `;
-
 const ProjectCard = styled.div`
   width: 80%;
-  /* max-width: 700px; */
-  /* margin: 20px; */
   padding: 20px;
   background-color: #fff;
   border-radius: 8px;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
   transition: transform 0.3s ease-in-out;
-
-  &:hover {
-    /* transform: scale(1.02); */
-  }
 `;
 
 const ProjectTitle = styled.h3`
@@ -96,7 +114,8 @@ const ProjectTitle = styled.h3`
 
 const ProjectDescription = styled.p`
   text-indent: 1em;
-  line-height: 1.2;
+  line-height: 1.6; /* Increase line height for better readability */
+  font-size: 1.2rem; /* Adjust font size as needed */
 `;
 
 const FeatureList = styled.ul`
@@ -113,10 +132,37 @@ const Feature = styled.li`
 
 const FeatureTitle = styled.strong`
   font-size: 1.1rem;
+  margin-left: 1em; /* Add space between feature title and description */
 `;
 
 const FeatureDescription = styled.span`
   font-size: 1rem;
+`;
+const floatAnimation = keyframes`
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-5px);
+  }
+`;
+
+const ScrollIndicator = styled.div`
+  position: fixed;
+  bottom: 50px;
+  right: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  cursor: pointer;
+  animation: ${floatAnimation} 1s ease-in-out infinite;
+  opacity: 0.7;
+  transition: opacity 0.3s ease;
+
+  &:hover {
+    opacity: 1;
+  }
 `;
 
 const ProjectLinks = styled.div`
@@ -148,10 +194,13 @@ const ExpandButton = styled.button`
   background-color: transparent;
   border: none;
   cursor: pointer;
-  color: blue;
+  color: #007bff; /* Use a contrasting color */
   font-weight: bold;
+  text-decoration: underline; /* Underline the button text */
+  transition: color 0.3s ease;
+
   &:hover {
-    text-decoration: underline;
+    color: #0056b3; /* Change color on hover */
   }
 `;
 
